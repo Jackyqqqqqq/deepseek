99c6ccce362fbfe970ab9c270e44ef1949bdfbb9
qyz <2391534566@qq.com>
2025-03-06 00:54:48 +0800

    commit

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,10 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+/// <reference types="next" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    DEEPSEEK_API_KEY: string;
+  }
+}
